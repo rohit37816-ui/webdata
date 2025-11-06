833dcf72a74f6307aa4ea5a5c283cc05496dbf45
rohit37816-ui <rohit37816@gmail.com>
2025-11-06 22:04:01 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 from flask import Flask
 from threading import Thread
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = os.getenv("7870355572:AAGVf8WkgkjvaYen_uqBSCT8TJCoihLbBXg")
 
 # --- Flask for Render Keep Alive ---
 app = Flask(__name__)
